6c53dd2e793a37de7e08d9d5204029a5bca0c4df
Alberto Duarte <betodute@gmail.com>
2023-10-04 16:43:22 -0700

    back to original

--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -9,7 +9,7 @@ $.ajaxSetup({
 export var indexTasks = function (successCB, errorCB) {
   var request = {
     type: 'GET',
-    url: `api/tasks/api_key=`,
+    url: `api/tasks/?api_key=1`,
     success: successCB,
     error: errorCB
   };
